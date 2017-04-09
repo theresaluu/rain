b741576235f78909f510a236eed2e5714733ea4d
Theresa Luu <theresa.luu@gmail.com>
2017-04-09 16:04:06 -0700

    used Accept header info instead of explicitly indicating the version in the url

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :emotions
     end
   end
