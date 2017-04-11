eaf9c04919d22766d63402f90bb9e006e025664d
Theresa Luu <theresa.luu@gmail.com>
2017-04-10 23:29:55 -0700

    access tokens added

--- app/controllers/api/v1/emotions_controller.rb
+++ app/controllers/api/v1/emotions_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class EmotionsController < ApplicationController
+      before_filter :restrict_access
 
       def create
         @emotion = Product.create(emotion_params)
@@ -28,6 +29,13 @@ module Api
         params.require(:emotion).permit(:recognize, :category_id, :user_id,
                                         :investigation, :action_item, :current)
       end
+
+      def restrict_access
+        authenticate_or_request_with_http_token do |token, options|
+          ApiKey.exists?(access_token: token)
+        end
+      end
+
     end
   end
 end
---
+++ db/migrate/20170410072214_create_api_keys.rb
@@ -0,0 +1,11 @@
+class CreateApiKeys < ActiveRecord::Migration[5.0]
+  def change
+    create_table :api_keys do |t|
+      t.string :access_token
+      t.string :role
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
